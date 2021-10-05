2c8a3237936a5d78f958c1ab36c2c2be30b65180
jtmccormick18 <josh.mccormick81@gmail.com>
2021-10-05 14:32:49 -0400

    changes

--- js/reports.js
+++ js/reports.js
@@ -1,37 +1,125 @@
 const CreatePDFfromHTML = function (filename) {
-    var HTML_Width = $(".html-content").width();
-    var HTML_Height = $(".html-content").height();
-    var top_left_margin = 15;
-    var PDF_Width = HTML_Width + top_left_margin * 2;
-    var PDF_Height = PDF_Width * 1.5 + top_left_margin * 2;
-    var canvas_image_width = HTML_Width;
-    var canvas_image_height = HTML_Height;
+  var HTML_Width = $(".html-content").width();
+  var HTML_Height = $(".html-content").height();
+  var top_left_margin = 15;
+  var PDF_Width = HTML_Width + top_left_margin * 2;
+  var PDF_Height = PDF_Width * 1.5 + top_left_margin * 2;
+  var canvas_image_width = HTML_Width;
+  var canvas_image_height = HTML_Height;
 
-    var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+  var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
 
-    html2canvas($(".html-content")[0]).then(function (canvas) {
-      var imgData = canvas.toDataURL("image/jpeg", 1.0);
-      var pdf = new jsPDF("p", "pt", [PDF_Width, PDF_Height]);
+  html2canvas($(".html-content")[0]).then(function (canvas) {
+    var imgData = canvas.toDataURL("image/jpeg", 1.0);
+    var pdf = new jsPDF("p", "pt", [PDF_Width, PDF_Height]);
+    pdf.addImage(
+      imgData,
+      "JPG",
+      top_left_margin,
+      top_left_margin,
+      canvas_image_width,
+      canvas_image_height
+    );
+    for (var i = 1; i <= totalPDFPages; i++) {
+      pdf.addPage(PDF_Width, PDF_Height);
       pdf.addImage(
         imgData,
         "JPG",
         top_left_margin,
-        top_left_margin,
+        -(PDF_Height * i) + top_left_margin * 4,
         canvas_image_width,
         canvas_image_height
       );
-      for (var i = 1; i <= totalPDFPages; i++) {
-        pdf.addPage(PDF_Width, PDF_Height);
-        pdf.addImage(
-          imgData,
-          "JPG",
-          top_left_margin,
-          -(PDF_Height * i) + top_left_margin * 4,
-          canvas_image_width,
-          canvas_image_height
-        );
+    }
+    pdf.save(filename || "Sample_Report_PDF");
+    // $(".html-content").hide();
+  });
+};
+
+const exportToExcel = function (filename) {
+  var titles = [];
+  var data = [];
+
+  /*
+   * Get the table headers, this will be CSV headers
+   * The count of headers will be CSV string separator
+   */
+  $(".table th").each(function () {
+    titles.push($(this).text());
+  });
+
+  /*
+   * Get the actual data, this will contain all the data, in 1 array
+   */
+  $(".table td").each(function () {
+    data.push($(this).text());
+  });
+
+  /*
+   * Convert our data to CSV string
+   */
+  var CSVString = prepCSVRow(titles, titles.length, "");
+  CSVString = prepCSVRow(data, titles.length, CSVString);
+
+  /*
+   * Make CSV downloadable
+   */
+  var downloadLink = document.createElement("a");
+  var blob = new Blob(["\ufeff", CSVString]);
+  var url = URL.createObjectURL(blob);
+  downloadLink.href = url;
+  downloadLink.download = filename+".csv"||"data.csv";
+
+  /*
+   * Actually download CSV
+   */
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+};
+
+/*
+ * Convert data array to CSV string
+ * @param arr {Array} - the actual data
+ * @param columnCount {Number} - the amount to split the data into columns
+ * @param initial {String} - initial string to append to CSV string
+ * return {String} - ready CSV string
+ */
+function prepCSVRow(arr, columnCount, initial) {
+  var row = ""; // this will hold data
+  var delimeter = ","; // data slice separator, in excel it's `;`, in usual CSv it's `,`
+  var newLine = "\r\n"; // newline separator for CSV row
+
+  /*
+   * Convert [1,2,3,4] into [[1,2], [3,4]] while count is 2
+   * @param _arr {Array} - the actual array to split
+   * @param _count {Number} - the amount to split
+   * return {Array} - splitted array
+   */
+  function splitArray(_arr, _count) {
+    var splitted = [];
+    var result = [];
+    _arr.forEach(function (item, idx) {
+      if ((idx + 1) % _count === 0) {
+        splitted.push(item);
+        result.push(splitted);
+        splitted = [];
+      } else {
+        splitted.push(item);
       }
-      pdf.save(filename || "Sample_Report_PDF");
-      // $(".html-content").hide();
     });
-  };
+    return result;
+  }
+  var plainArr = splitArray(arr, columnCount);
+  // don't know how to explain this
+  // you just have to like follow the code
+  // and you understand, it's pretty simple
+  // it converts `['a', 'b', 'c']` to `a,b,c` string
+  plainArr.forEach(function (arrItem) {
+    arrItem.forEach(function (item, idx) {
+      row += item + (idx + 1 === arrItem.length ? "" : delimeter);
+    });
+    row += newLine;
+  });
+  return initial + row;
+}
